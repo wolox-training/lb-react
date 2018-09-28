2885e1cf7adae140d6a242daee5b71375e9e9977
Laucha <laucha.bo@gmail.com>
2018-09-28 14:44:57 -0300

    redux forms wip

--- tic-tac-toe/src/redux/store.js
+++ tic-tac-toe/src/redux/store.js
@@ -1,10 +1,27 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import { reducer } from './tic-tac-toe/reducer';
 
+const rootReducer = combineReducers({
+  reducer,
+  form: formReducer
+});
+
+const store = createStore(rootReducer);
+
 /* eslint-disable no-underscore-dangle */
 export default createStore(
-  reducer,
+  store,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 /* eslint-enable */
+
+
+
+Componente onSubmit=...rootReducer
+
+
+
+
+Componente 0
