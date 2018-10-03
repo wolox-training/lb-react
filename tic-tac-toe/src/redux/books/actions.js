218ff8c672a03c3252cfaf3f58e57b4d6f74fcbc
Laucha <laucha.bo@gmail.com>
2018-10-03 12:54:31 -0300

    middleware wip

---
+++ tic-tac-toe/src/redux/books/actions.js
@@ -0,0 +1,23 @@
+import BookService from '../../services/BookService';
+
+export const actions = createTypes(['GET_BOOKS', 'GET_BOOKS_SUCCESS', 'GET_BOOKS_FAILURE'], '@@BOOKS');
+
+const actionCreators = {
+  getBooks: () => async dispatch => {
+    dispatch({ type: actions.GET_BOOKS });
+    const response = await BookService.getBooks();
+    if (response.ok) {
+      dispatch({
+        type: actions.GET_BOOKS_SUCCESS,
+        payload: response.data
+      });
+    } else {
+      dispatch({
+        type: actions.GET_BOOKS_FAILURE,
+        payload: response.problem
+      });
+    }
+  }
+};
+
+export default actionCreators;
--- tic-tac-toe/src/redux/store.js
+++ tic-tac-toe/src/redux/store.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import thunk from 'redux-thunk';
 
 import { reducer } from './tic-tac-toe/reducer';
 
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
 /* eslint-disable no-underscore-dangle */
 export default createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 /* eslint-enable */
